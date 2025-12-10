b4b4e11c00c398195e948ab687eb68266cbd893b
ysagar558 <ysagar558@gmail.com>
2025-12-10 14:18:53 +0530

    Displaying products on screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
+
+const productsArr = [
+
+{
+
+title: 'Colors',
+
+price: 100,
+
+imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+
+},
+
+{
+
+title: 'Black and white Colors',
+
+price: 50,
+
+imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+
+},
+
+{
+
+title: 'Yellow and Black Colors',
+
+price: 70,
+
+imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+
+},
+
+{
+
+title: 'Blue Color',
+
+price: 100,
+
+imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
+
+}
+
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {productsArr.map((item)=>(
+      <div>
+        <img src={item.imageUrl} alt={item.title}/>
+        <h3>{item.title}</h3>
+        <h3>${item.price}</h3>
+        <Button>Add To Cart</Button>
+      </div>
+    )
+
+    )}
+    
+    </>
+
   );
 }
 
